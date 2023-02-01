ff5000b3cb1359e7acd3386b9a384b5a747ecb95
reinerkp <kate.reiner1@gmail.com>
2023-01-31 21:31:32 -0700

    Fixed local storage so that values are retrieved and placed into each hour block after refresh.

--- Develop/script.js
+++ Develop/script.js
@@ -49,22 +49,21 @@ $(document).ready(function() {
         $(this).removeclass("past");
         $(this).removeclass("present");
         $(this).addclass("future");
-      }
-
-      // Retrieves value entered into local storage
-      $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-      $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-      $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-      $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-      $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-      $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-      $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-      $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-      $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-      $("#hour-18 .description").val(localStorage.getItem("hour-18"));
-   
+      }   
 
     });
+
+    // Retrieves value entered into local storage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
 
 
 
